8b09541d5b285b6bfd8f0c5ce881799446b31970
António Rego <up202108666@fe.up.pt>
2024-12-06 16:51:32 +0000

    Functional client-side list manipulation and saving, some server-side features added

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -7,7 +7,6 @@
 #include <fstream>
 #include "../server/handler.hpp"
 
-
 md5 encrypter1;
 
 enum states {
@@ -18,6 +17,37 @@ enum states {
 };
 
 states state = NO_LIST;
+std::string user_id;
+json j;
+
+void loadUser(){
+    std::ifstream file2("server/number.json");
+    file2 >> j;
+    file2.close();
+
+    // check if there is a info.json file in the client dir, if there is not, create one and put hashed id in it, else read the hashed id from the file
+    std::ifstream file("client/info.json");
+    if (!file.good()) {
+        std::ofstream file("client/info.json");
+        // Check the number.json file in the server directory and get the value of the number key, hash it to use as the hashed id and increment the number value by 1
+        int number = j["number"];
+        j["number"] = number + 1;
+        user_id = encrypter1.encrypt(std::to_string(number));
+        // Write the user_id to the info.json file with the key user_id
+        json user_id_json = {{"user_id", user_id}};
+        file << user_id_json;
+        file.close();
+        // Add the new user to user_numbers
+        json new_user = {{user_id, 1}};
+        j["user_numbers"].push_back(new_user);
+
+    } else {
+        json client_json;
+        file >> client_json;
+        user_id = client_json["user_id"];
+        file.close();
+    }
+}
 
 vector<ShoppingList> loadLists(){
     std::string listFolder = "./client/lists";
@@ -51,18 +81,67 @@ vector<ShoppingList> loadLists(){
     return shopping_lists;
 }
 
-int createList(){
+int createList(vector<ShoppingList>& shopping_lists, zmq::socket_t& socket){
+    // Update user counter
+    int value;
+    bool user_found = false;
+    // is a for loop really needed? can't we just check if the user_id is in the json object?
+    for (auto& user : j["user_numbers"]) { 
+        if (user.contains(user_id)) {     
+            user[user_id] = user[user_id].get<int>() + 1; 
+            value = user[user_id].get<int>();
+            user_found = true;
+            break;
+        }
+    }
+
+    if (!user_found) {
+        json new_user = {{user_id, 1}};
+        value = 1;
+        j["user_numbers"].push_back(new_user);
+    }
+
+    // Save the updated number.json file
+    // this routine seems dangerous, should this not be handled by the server?
+    // i.e. send a request for creating the userid?
+    std::ofstream out_file("server/number.json");
+    out_file << j.dump(4);
+    out_file.close();
+
+    std::string unhashed_list = user_id + "-" + std::to_string(value);
+    std::string list_id = encrypter1.encrypt(unhashed_list);
+
+    // create list object and store in memory
+    ShoppingList shopping_list(list_id);
+    shopping_list.setUserId(user_id);
+    shopping_list.fresh();
+    shopping_lists.push_back(shopping_list);
+
+    // save to machine
+    std::ofstream file("./client/lists/" + list_id + ".json");
+    file << shopping_list.contentsToJSON();
+    file.close();
+
+    std::cout << "Created list with id: " << list_id << std::endl;
+
+    // send to server
+    Message creation("create", list_id, {});
+    s_send(socket, creation.toString());
+
+    // receive reply from server
+    // todo once we actally send errors when stuff goes wrong on server
+
     return 0;
 }
 
-states mainMenuUI(){
-    std::cout << "1: Create a new list" << std::endl;
+states mainMenuUI(vector<ShoppingList>& shopping_lists, zmq::socket_t& socket){
+    std::cout << std::endl << "1: Create a new list" << std::endl;
     std::cout << "2: Select a list" << std::endl;
     std::cout << "3: Exit" << std::endl;
     int selection;
     std::cin >> selection;
     if (selection == 1){
-        createList();
+        createList(shopping_lists, socket);
         return NO_LIST;
     }
     else if (selection == 2){
@@ -77,8 +156,8 @@ states mainMenuUI(){
     }
 }
 
-ShoppingList* selectListUI(vector<ShoppingList> shopping_lists){
-    std::cout << "Select a list: " << std::endl;
+ShoppingList* selectListUI(vector<ShoppingList>& shopping_lists){
+    std::cout << std::endl << "Select a list: " << std::endl;
     for (int i = 0; i < shopping_lists.size(); i++){
         std::cout << i << ": " << shopping_lists[i].get_id() << std::endl;
     }
@@ -95,7 +174,7 @@ ShoppingList* selectListUI(vector<ShoppingList> shopping_lists){
 }
 
 int alterListUI(ShoppingList* shoppingList){
-    std::cout << shoppingList->print() << std::endl;
+    std::cout << std::endl << shoppingList->print() << std::endl;
     std::cout << "1: Add an item" << std::endl;
     std::cout << "2: Remove an item" << std::endl;
     std::cout << "3: Change the quantity of an item" << std::endl;
@@ -112,7 +191,7 @@ int alterListUI(ShoppingList* shoppingList){
         std::string item;
         std::cout << "Enter the item to remove: ";
         std::cin >> item;
-        shoppingList->reset(item);
+        shoppingList->decrease(item);
     }
     else if (selection == 3){
         std::string item;
@@ -124,7 +203,10 @@ int alterListUI(ShoppingList* shoppingList){
         shoppingList->set_value(item, quantity);
     }
     else if (selection == 4){
-        // saving routine here?
+        // saving to machine routine, it's pretty simple but we could wrap it in a function for clarity
+        std::ofstream file("./client/lists/" + shoppingList->get_id() + ".json");
+        file << shoppingList->contentsToJSON();
+        file.close();
         shoppingList = nullptr;
         state = NO_LIST;
     }
@@ -142,61 +224,10 @@ int main() {
 
     socket.connect("tcp://localhost:5559");
 
-    std::string user_id;
-    std::ifstream file2("server/number.json");
-    json j;
-    file2 >> j;
-    file2.close();
+    // Load the user id
+    loadUser();
 
-    // check if there is a info.json file in the client dir, if there is not, create one and put hashed id in it, else read the hashed id from the file
-    std::ifstream file("client/info.json");
-    if (!file.good()) {
-        std::ofstream file("client/info.json");
-        // Check the number.json file in the server directory and get the value of the number key, hash it to use as the hashed id and increment the number value by 1
-        int number = j["number"];
-        j["number"] = number + 1;
-        user_id = encrypter1.encrypt(std::to_string(number));
-        // Write the user_id to the info.json file with the key user_id
-        json user_id_json = {{"user_id", user_id}};
-        file << user_id_json;
-        file.close();
-        // Add the new user to user_numbers
-        json new_user = {{user_id, 1}};
-        j["user_numbers"].push_back(new_user);
-
-    } else {
-        json client_json;
-        file >> client_json;
-        user_id = client_json["user_id"];
-        file.close();
-    }
-
-    // Update user counter
-    int value;
-    bool user_found = false;
-    for (auto& user : j["user_numbers"]) { 
-        if (user.contains(user_id)) {     
-            user[user_id] = user[user_id].get<int>() + 1; 
-            value = user[user_id].get<int>();
-            user_found = true;
-            break;
-        }
-    }
-
-    if (!user_found) {
-        json new_user = {{user_id, 1}};
-        value = 1;
-        j["user_numbers"].push_back(new_user);
-    }
-
-    // Save the updated number.json file
-    std::ofstream out_file("server/number.json");
-    out_file << j.dump(4);
-    out_file.close();
-
-    std::string unhashed_list = user_id + "-" + std::to_string(value);
-    std::string list_id = encrypter1.encrypt(unhashed_list);
-
+/*
     Message creation("create", list_id, {});
     s_send(socket, creation.toString());
 
@@ -228,6 +259,7 @@ int main() {
         // Print the received reply
         std::cout << "Received reply " << request_nbr << " [" << reply_str << "]" << std::endl;
     }
+*/
 
     vector<ShoppingList> shopping_lists = loadLists();
     ShoppingList *current_shopping_list = nullptr;
@@ -235,7 +267,7 @@ int main() {
     while (true){
         switch (state){
             case NO_LIST:
-                state = mainMenuUI();
+                state = mainMenuUI(shopping_lists, socket);
                 break;
             case SELECTING_LIST:
                 current_shopping_list = selectListUI(shopping_lists);
