f891a9c2014008be206a5e8052c767dddc98049b
up202108806 <up202108806@edu.fe.up.pt>
2024-12-08 18:58:23 +0000

    Add test file for CRDT

--- src/crdt/body/ShoppingList.cpp
+++ src/crdt/body/ShoppingList.cpp
@@ -46,7 +46,7 @@ void ShoppingList::decrease(string item, int n){
 
   int quantity = items.get_quantity(item);
   if (quantity > n)
-    items.decrease(item, user_id);
+    items.decrease(item, n, user_id);
   else
     items.decrease(item, quantity, user_id);
 }
@@ -90,6 +90,12 @@ void ShoppingList::fresh(){
 ShoppingList ShoppingList::merge(ShoppingList other){
   ShoppingList new_shopping_list(this->id);
   new_shopping_list.items = this->items.merge(other.items, this->user_id);
+
+  for (auto &item : new_shopping_list.items.get_items_with_quantity()){
+    if (item.second < 0)
+      new_shopping_list.items.remove(item.first, this->user_id);
+  }
+
   return new_shopping_list;
 }
 
@@ -126,50 +132,3 @@ ShoppingList::ShoppingList(string id, json j){
   this->id = id;
   this->items = CounterMap(j);
 }
-
-// int main(){
-//   string user_id = "1";
-//   string user_id2 = "2";
-//   ShoppingList shopping_list("1");
-//   shopping_list.setUserId(user_id);
-
-//   cout << "Original shopping list: " << endl;
-//   shopping_list.add("apple");
-//   shopping_list.add("banana");
-//   shopping_list.add("apple", 3);
-//   shopping_list.add("pinapple");
-
-//   cout << shopping_list.print() << endl
-//        << endl;
-
-//   ShoppingList shopping_list_copy = shopping_list.copy();
-//   shopping_list_copy.setUserId(user_id2);
-//   shopping_list_copy.add("orange");
-//   shopping_list_copy.add("apple", 2);
-//   shopping_list_copy.add("banana", 20);
-//   shopping_list_copy.decrease("banana", 21);
-//   shopping_list_copy.remove("pinapple");
-
-//   shopping_list.decrease("apple");
-//   shopping_list.decrease("banana", 2);
-//   shopping_list.add("pinapple");
-
-//   cout << "Shopping list after user 1:" << endl;
-//   cout << shopping_list.print() << endl
-//        << endl;
-
-//   cout << "Shopping list after user 2:" << endl;
-//   cout << shopping_list_copy.print() << endl
-//        << endl;
-
-//   // cout << shopping_list_copy.contentsToJSON().dump(4) << endl;
-
-//   cout << "Merged shopping list:" << endl;
-//   cout << shopping_list.merge(shopping_list_copy).print() << endl
-//        << endl;
-       
-//   // ShoppingList jsonShopping("123", shopping_list_copy.contentsToJSON());
-
-//   // cout << "JSON" << endl;
-//   // cout << jsonShopping.contentsToJSON().dump(4) << endl;
-// }
---
+++ src/test/test.cpp
@@ -0,0 +1,76 @@
+#include "../crdt/headers/ShoppingList.hpp"
+
+int testShoppingList(){
+  string user_id = "1";
+  string user_id2 = "2";
+  ShoppingList shopping_list("1");
+  shopping_list.setUserId(user_id);
+
+  cout << "Original shopping list: " << endl;
+  shopping_list.add("apple");
+  shopping_list.add("banana", 2);
+  shopping_list.add("apple", 3);
+  shopping_list.add("pinapple");
+  shopping_list.add("kiwi");
+  shopping_list.add("mango");
+
+  cout << shopping_list.print() << endl
+       << endl;
+
+  ShoppingList shopping_list_copy = shopping_list.copy();
+  shopping_list_copy.setUserId(user_id2);
+  shopping_list_copy.add("orange");
+  shopping_list_copy.add("apple", 2);
+  shopping_list_copy.add("banana", 20);
+  shopping_list_copy.decrease("banana", 21);
+  shopping_list_copy.remove("pinapple");
+  shopping_list_copy.remove("mango");
+
+  shopping_list.decrease("apple");
+  shopping_list.decrease("banana", 2);
+  shopping_list.add("pinapple");
+
+  cout << "Shopping list after user 1:" << endl;
+  cout << shopping_list.print() << endl
+       << endl;
+
+  cout << "Shopping list after user 2:" << endl;
+  cout << shopping_list_copy.print() << endl
+       << endl;
+
+  // cout << shopping_list_copy.contentsToJSON().dump(4) << endl;
+  ShoppingList merged_shopping_list = shopping_list.merge(shopping_list_copy);
+
+  cout << "Merged shopping list:" << endl;
+  cout << merged_shopping_list.print() << endl
+       << endl;
+
+  assert(merged_shopping_list.get_quantity("apple") == 5); 
+
+  assert(merged_shopping_list.get_quantity("banana") == 0);
+
+  assert(merged_shopping_list.get_quantity("pinapple") == 1);
+
+  assert(merged_shopping_list.contains("orange") == true);
+
+  assert(merged_shopping_list.contains("mango") == false);
+
+  assert(merged_shopping_list.get_quantity("kiwi") == 1);
+
+  ShoppingList dummy_shopping_list("123", merged_shopping_list.contentsToJSON());
+
+  assert(dummy_shopping_list.get_quantity("apple") == 5);
+
+  return 0;
+}
+
+
+int main(){
+  if (testShoppingList() == 0){
+    cout << "All ShoppingList tests passed" << endl;
+  } else {
+    cout << "Some ShoppingList tests failed" << endl;
+  }
+
+  return 0;
+}
